4dc5668121e935990117f7d74fe836adb4cac2c3
Momentum <momentumdigital@Momentums-MacBook-Air.local>
2021-05-24 11:47:30 +0530

    [12345] - Crousel change

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -13,7 +13,7 @@
 	</p>
 
 	<h1>Heading two</h1>
-	<p>If your computer already has PHP and Composer installed, you may create a new Laravel project by using Composer directly. After the application has been created, you may start Laravel's local development server using the Artisan CLI's serve command:
+	<p>If your computer already has PHP and Composer installed. You may create a new Laravel project by using Composer directly. After the application has been created, you may start Laravel's local development server using the Artisan CLI's serve command:
 	<a href="/post">click here for details</a></p>
 	<h1>Heading three </h1>
 	<p>If your computer already has PHP and Composer installed, you may create a new Laravel project by using Composer directly. After the application has been created, you may start Laravel's local development server using the Artisan CLI's serve command:
